73e64885f2aab97b341db6a1c37b9e6e33934b7e
Srigowri Deogirikar <32318383+srigowrideo@users.noreply.github.com>
2021-07-08 10:01:16 +0530

    Add files via upload

---
+++ classtest2.java
@@ -0,0 +1,25 @@
+package fibonacciseries;
+
+public class classtest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] numbers= {10,20,30,40,50};
+for(int x:numbers)
+	{
+	System.out.print(x);
+	System.out.print(",");
+	
+System.out.print("\n");
+	}
+int i=0;
+String a[]={"James","Larry","Laucy","Tommy"};
+	for(String names=a[i];i<=3;i++) {
+		 System.out.print(a[i]);
+		 System.out.print(",");
+	 }
+	 }
+}
+	 
+
+
